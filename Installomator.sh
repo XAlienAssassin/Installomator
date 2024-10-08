29e2754f2118b95dca855cabf77b16650c6b4219
Orion Medina <31485978+XAlienAssassin@users.noreply.github.com>
2024-10-08 13:57:49 -0400

    Added VitalSource Blackbaud

--- Installomator.sh
+++ Installomator.sh
@@ -8460,6 +8460,16 @@ virtualbuddy)
     appNewVersion=$(versionFromGit insidegui VirtualBuddy)
     expectedTeamID="8C7439RJLG"
     ;;
+vitalsourcebookshelf)
+    name="VitalSource Bookshelf"
+    type="dmg"
+    # Get the latest version directory
+    downloadURL=$(curl -fsL -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36" "https://support.vitalsource.com/hc/en-us/articles/360014107913-Mac" | grep -i "mac/bookshelf/" | head -1 | grep -o -i -E "https.*" | cut -d '"' -f1)
+    # Set the app new version
+    appNewVersion=$( echo "${downloadURL}" | sed -E 's/.*VitalSource-Bookshelf_([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\.dmg/\1/')
+    expectedTeamID="QX549BPYZ8"
+    blockingProcesses=( vitalsourcebookshelf )
+    ;;
 viscosity)
     #credit: @matins
     name="Viscosity"
